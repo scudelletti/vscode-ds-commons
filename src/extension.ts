73d5d7abc017862844de6d3c270f3a4fb67c63b1
Diogo Scudelletti <diogo@scudelletti.com>
2023-06-09 17:44:28 +0200

    Implement test runners

--- src/extension.ts
+++ src/extension.ts
@@ -19,8 +19,58 @@ export function activate(context: vscode.ExtensionContext) {
     setTerminalFontSize((n) => n - 1);
   });
 
+  let dsRunTestFile = vscode.commands.registerCommand('ds-commons.ds.action.runTestFile', () => {
+    const terminal = findOrCreateTerminal("ds-output");
+    terminal.show();
+    terminal.sendText("reset");
+
+    const [file, _] = getFileAndLine();
+    const command = generateTestCommand(file);
+
+    terminal.sendText(command);
+  });
+
+  let dsRunTestCursor = vscode.commands.registerCommand('ds-commons.ds.action.runTestCursor', () => {
+    const terminal = findOrCreateTerminal("ds-output");
+    terminal.show();
+    terminal.sendText("reset");
+
+    const [file, lineNumber] = getFileAndLine();
+    const command = generateTestCommand(`${file}:${lineNumber}`);
+
+    terminal.sendText(command);
+  });
+
+  let generateTestCommand = (file : string) => {
+    const prefix = 'MIX_ENV=test iex -S mix do test';
+    const suffix = '--trace , run -e "System.halt"';
+
+    return `${prefix} ${file} ${suffix}`;
+  };
+
+  let getFileAndLine = () =>{
+    const editor = vscode.window.activeTextEditor;
+
+    const fileName = editor!.document.fileName;
+    const lineNumber = editor!.selection.active.line + 1;
+
+    return [fileName as string, lineNumber.toString()];
+  };
+
+  let findOrCreateTerminal = (terminalName : string) => {
+    let terminal = vscode.window.terminals.find(terminal => terminal.name === terminalName);
+
+    if (!terminal) {
+      terminal = vscode.window.createTerminal("ds-output");
+    }
+
+    return terminal;
+  };
+
   context.subscriptions.push(terminalZoomIn);
   context.subscriptions.push(terminalZoomOut);
+  context.subscriptions.push(dsRunTestFile);
+  context.subscriptions.push(dsRunTestCursor);
 }
 
 export function deactivate() {}
